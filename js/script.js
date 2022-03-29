1a0f902f4673f51c4fbeeb874983ed4c1d3066da
pasathbandara <101523910+pasathbandara@users.noreply.github.com>
2022-03-29 16:34:58 +0530

    bug fixes

--- js/script.js
+++ js/script.js
@@ -8,29 +8,31 @@ form.addEventListener('submit', (e) => {
   let messages = []
   if (email.value === '' || email.value == null)  {
     window.alert('Email is required')
-    break;
+    return false;
   }
   if (name.value === '' || name.value == null) {
     window.alert('Name is required')
+    return false;
   }
 
   if (pwd.value.length <= 6) {
     window.alert('pwd must be longer than 6 characters')
-    break;
+    return false;
   }
 
   if (pwd.value.length >= 20) {
     window.alert('pwd must be less than 20 characters')
-    break;
+    return false;
   }
 
   if (pwd.value === 'pwd') {
     window.alert('password cannot be password')
-    break;
+    return false;
   }
 
   if (messages.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
+    return false;
   }
 })
